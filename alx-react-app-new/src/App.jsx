73f86733ba9c1fbe590b6a359f1605b1ae7d4551
joe dee <joedee4311@gmail.com>
2025-07-20 22:02:23 +0300

    updated

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 function App() {
 
 
@@ -16,6 +17,8 @@ function App() {
       <h1>User Profiles</h1>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="Bob" age="30" bio="An avid traveler and foodie" />
+      <h1 style={{ textAlign: 'center' }}>Simple Counter App</h1>
+      <Counter />
     </>
   )
 }
